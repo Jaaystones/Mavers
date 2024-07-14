7faec018f19cb3014afa1cdd2da2a47a063f6bbc
Jaaystones <Joelpaul345@gmail.com>
2024-07-14 18:47:00 +0100

    uppdated codes

--- Backend/config/allowCors.js
+++ Backend/config/allowCors.js
@@ -1,8 +1,8 @@
-const allowOrigins = require('./allowOrigins')
+const allowdOrigins = require('./allowOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowOrigins.indexOf(origin) !== -1 || !origin) {
+        if (allowdOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
